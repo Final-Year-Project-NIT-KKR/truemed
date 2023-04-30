c230100b9e1dadfe6d4a6f826c959397efc234c0
tusharginotra <tusharginotra2000@gmail.com>
2023-04-30 15:23:19 +0530

    added jsqr library

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import StartingPage from './components/StartingPage';
 import BusinessDrawer from './components/BusinessDrawer';
 import CreateShipment from './components/CreateShipment';
 import PendingShipment from './components/PendingShipment';
+import VerifyShipment from './components/VerifyShipment';
 const theme = createTheme({
   palette: {
     primary: {
@@ -25,12 +26,14 @@ function App() {
   return <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <StartingPage/> } />
-        <Route path="/admin" element={<ResponsiveDrawer componentToPass={<MedicineTable/>}  />} />
+      <Route path="/" element={ <VerifyShipment /> } />
+
+        {/* <Route path="/" element={ <StartingPage/> } /> */}
+        {/* <Route path="/admin" element={<ResponsiveDrawer componentToPass={<MedicineTable/>}  />} />
         <Route path="/admin/users" element={<ResponsiveDrawer componentToPass={<HomeAdmUsers/>}  />} />
         <Route path="/admin/newMed" element={<ResponsiveDrawer componentToPass={<AddNewMed/>}  />} />
         <Route path="/business" element={ <BusinessDrawer componentToPass={<CreateShipment/>} /> } />
-        <Route path="/business/pendingShip" element={ <BusinessDrawer componentToPass={<PendingShipment />} /> } />
+        <Route path="/business/pendingShip" element={ <BusinessDrawer componentToPass={<PendingShipment />} /> } /> */}
           {/* <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
---
+++ src/components/VerifyShipment.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Button, Stack } from '@mui/material'
+import { useState } from 'react';
+import JSQR from 'jsqr';
+
+
+function VerifyShipment() {
+
+  const [qrData, setQrData] = useState('No Result');
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const image = new Image();
+      image.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        const context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0, image.width, image.height);
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        const code = JSQR(imageData.data, imageData.width, imageData.height);
+        if (code) {
+          setQrData(code.data);
+        } else {
+          setQrData('No QR code found.');
+        }
+      };
+      image.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  };
+  return (
+    <div>
+      
+        <Stack direction="column" alignItems="center" spacing={2}>
+        
+        <input type="file" accept="image/*"  onChange={handleFileUpload}/>
+        {/* <Button onClick={handleFileUpload} variant="contained" component="label">
+        Upload
+        </Button> */}
+        <p>{qrData}</p>
+        </Stack>
+    
+    </div>
+  )
+}
+
+export default VerifyShipment
